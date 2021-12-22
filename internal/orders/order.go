341cbf704c34e0cf507b39133804b7453f8ec96f
Azzam Jihad Ulhaq <azzam.jiul@gmail.com>
2021-12-22 08:01:18 +0700

    get order items

--- internal/orders/order.go
+++ internal/orders/order.go
@@ -5,13 +5,20 @@ type Order struct {
 	ID          uint64 `json:"id"`
 	UserID      uint64 `json:"user_id"`
 	Status      int    `json:"status"`
-	Total       string `json:"total"`
+	Total       uint64 `json:"total"`
 	ExpiredDate string `json:"expired_date"`
+}
+
+// Order Item Element Request
+type OrderItem struct {
+	ItemID   uint64 `json:"item_id"`
+	Quantity int    `json:"quantity"`
+	Price    int    `json:"price"`
 }
 
 // Create Order Request
 type CreateOrderRequest struct {
 	Order
-
+	Items     []OrderItem
 	CreatedAt string `json:"created_at"`
 }
--- internal/orders/repo.go
+++ internal/orders/repo.go
@@ -1,11 +1,12 @@
 package orders
 
 import (
+	"fmt"
 	"online-store-go/pkg/error_utils"
 )
 
 const (
-	queryCreateOrder = "INSERT INTO orders (user_id, status, )"
+// queryCreateOrder = "INSERT INTO orders (user_id, status, )"
 )
 
 type repo struct {
@@ -20,11 +21,30 @@ func NewRepo() Repo {
 }
 
 func (r *repo) CreateOrder(data map[string]interface{}) *error_utils.RestErr {
-	// request := CreateOrderRequest{}
+	request := CreateOrderRequest{}
 
+	// Order properties
 	if data["user_id"] == nil {
 		return error_utils.NewBadRequestError("user_id is missing")
 	}
+	request.UserID = uint64(data["user_id"].(float64))
 
+	if data["total"] == nil {
+		return error_utils.NewBadRequestError("total is missing")
+	}
+	request.Total = uint64(data["total"].(float64))
+
+	// Order Items
+	for _, item := range data["items"].([]interface{}) {
+		orderItem := OrderItem{
+			ItemID:   uint64((item.(map[string]interface{})["item_id"]).(float64)),
+			Quantity: int((item.(map[string]interface{})["quantity"]).(float64)),
+			Price:    int((item.(map[string]interface{})["price"]).(float64)),
+		}
+
+		request.Items = append(request.Items, orderItem)
+	}
+
+	fmt.Println(request)
 	return nil
 }
